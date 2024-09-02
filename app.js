98a0a5aba019bfd45ddc72f801411e37bca8feb9
Amal K Reji <amalreji45.amal@gmail.com>
2024-09-02 16:34:41 +0530

    added session

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 
 const express = require('express')
 const hbs = require('hbs');
+const session = require('express-session');
+const cache = require('nocache');
+const nocache = require('nocache');
 
 const username = 'admin';
 const password = 'admin123';
@@ -12,21 +15,50 @@ const app = express();
 
 /// VIEW ENGINE /////////////////////
 app.use(express.static('public'));
-app.set('view engine','hbs');
+app.set('view engine', 'hbs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// app.use(session({secret:'key',cookie:{maxAge:600000}}));
+app.use(session({
+    secret: 'key',
+    resave: false,
+    saveUninitialized: true
+}));
+app.use(nocache());
 
-app.get('/',(req,res)=>{
-    res.render('login')
+
+app.get('/', (req, res) => {
+    if (req.session.user) {
+        res.redirect('home')
+    } else {
+        if (req.session.err) {
+            res.render('login', { msg: 'Invalid Username or Password' });
+            req.session.err = false;
+        }
+        res.render('login')
+    }
 })
 
-app.post('/verify',(req,res)=>{
-    console.log(req.body);
-    
-    res.send('success')
+app.post('/verify', (req, res) => {
+    if (req.body.username == username && req.body.password == password) {
+        req.session.user = req.body.username;
+        res.redirect('/home');
+    } else {
+        req.session.err = true;
+        res.redirect('/');
+    }
+})
+
+app.get('/home', (req, res) => {
+    if (req.session.user) {
+        res.render('home');
+    } else {
+        res.render('login', { msg: 'Invalid Username or Password' });
+    }
 })
 
 
 
-app.listen(3000,() =>console.log('Server listening on port 3000'));
+
+app.listen(3000, () => console.log('Server listening on port 3000'));
