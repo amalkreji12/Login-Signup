182d53ef38d00d9692f3949e30dfdbc4ce6895f9
Amal K Reji <amalreji45.amal@gmail.com>
2024-09-02 15:16:58 +0530

    added login page and route

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
+
+
+
 const express = require('express')
+const hbs = require('hbs');
+
+const username = 'admin';
+const password = 'admin123';
+
 const app = express();
 
 
+/// VIEW ENGINE /////////////////////
+app.use(express.static('public'));
+app.set('view engine','hbs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/',(req,res)=>{
+    res.render('login')
+})
+
+app.post('/verify',(req,res)=>{
+    console.log(req.body);
+    
+    res.send('success')
+})
 
 
 
